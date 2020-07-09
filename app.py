263a7f66840b4bc996e47bc404111febac19109a
vladislavbondarenko <bondarencko40@gmail.com>
2020-07-09 19:12:57 +0300

    Basic Mongo setup and test end-point

--- app.py
+++ app.py
@@ -1,56 +1,35 @@
-"""
-Flask Documentation:     http://flask.pocoo.org/docs/
-Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
-Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
-
-This file creates your application.
-"""
-
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
+from flask_cors import CORS
+from flask_pymongo import PyMongo
+from flask import jsonify
+
+import json
+from bson import json_util
+
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
+CORS(app)
 
-###
-# Routing for your application.
-###
+app.config["MONGO_URI"] = "mongodb+srv://vlad123:qwerty123@tic-tac-toe-jtwsd.mongodb.net/pizza-stock?retryWrites=true&w=majority"
+app.config['SECRET_KEY'] = 'hero'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def home():
-    """Render website's home page."""
-    return render_template('home.html')
+    return 'Deployed'
 
-
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
-
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
+@app.route("/get-stock")
+def getStock():
+    stock = mongo.db["pizza-stock"]
+    stock_list = list(stock.find())
+    return json.dumps(stock_list, default=json_util.default)
 
 @app.errorhandler(404)
 def page_not_found(error):
